d3790df9055432098ec64651ead534522442fe7e
Igor <igorsadovsky@gmail.com>
2025-02-06 17:56:59 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { useAppDispatch,useAppSelector } from './hooks'
+import { useAppSelector } from './hooks'
 import { Item } from './components/Item';
 
 function App() {

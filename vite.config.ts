7488d7253ab7f85ddf362719535034f55bb9a1cd
Igor <igorsadovsky@gmail.com>
2025-02-06 18:10:20 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://igorsadovsky1995.github.io/my-gym/', // Добавьте этот параметр
   plugins: [react()],
-})
+});
